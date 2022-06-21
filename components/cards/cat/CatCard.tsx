c7ea49d8a8f4cfbe79dea7f38c847b31dd33d825
basilsv <basilsv@gmail.com>
2022-06-21 00:55:15 -0400

    feat: create Catcard

---
+++ components/cards/cat/CatCard.tsx
@@ -0,0 +1,55 @@
+import styles from './CatCard.module.css';
+
+import Image from 'next/image';
+
+export interface ICatCard {
+  tag: string;
+  title: string;
+  body: string;
+  author: string;
+  time: string;
+}
+
+const CatCard: React.FC<ICatCard> = ({ tag, title, body, author, time }) => {
+  const srcGuy = `https://i.pravatar.cc/40?img=3`;
+  const srcCat = '/time-cat.jpg';
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <div className={styles.card__header}>
+          <Image
+            loader={() => srcCat}
+            src={srcCat}
+            alt="card__image"
+            className={styles.card__image}
+            width="600"
+            height="400"
+          />
+        </div>
+        <div className={styles.card__body}>
+          <span className={`${styles.tag} ${styles['tag-blue']}`}>{tag}</span>
+          <h4>{title}</h4>
+          <p>{body}</p>
+        </div>
+        <div className={styles.card__footer}>
+          <div className={styles.user}>
+            <Image
+              loader={() => srcGuy}
+              src={srcGuy}
+              alt="user__image"
+              className={styles.user__image}
+              width="40"
+              height="40"
+            />
+            <div className={styles.user__info}>
+              <h5>{author}</h5>
+              <small>{time}</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CatCard;
